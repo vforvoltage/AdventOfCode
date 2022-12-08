1ced42cab2965b21b7b1ab1e82dbf9b56f74ec14
vforvoltage <vforvoltage@gmail.com>
2022-12-08 15:33:25 -0500

    fix test names to actually run with the build

--- src/test/java/org/vforvoltage/adventofcode/year2022/TestDays2022.java
+++ src/test/java/org/vforvoltage/adventofcode/year2022/TestDays2022.java
@@ -1,15 +1,15 @@
-package org.vforvoltage.adventofcode.year2015;
+package org.vforvoltage.adventofcode.year2022;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ArgumentsSource;
 import org.vforvoltage.adventofcode.common.Day;
 
-public class DayTests2015 {
+public class TestDays2022 {
 
     @ParameterizedTest
-    @ArgumentsSource(DaySolutionArgumentProvider2015.class)
-    public void testDays2015(Day day, Object part1Expected, Object part2Expected) {
+    @ArgumentsSource(DaySolutionArgumentProvider2022.class)
+    public void testDays2022(Day day, Object part1Expected, Object part2Expected) {
         Assertions.assertEquals(part1Expected, day.part1());
         Assertions.assertEquals(part2Expected, day.part2());
     }
