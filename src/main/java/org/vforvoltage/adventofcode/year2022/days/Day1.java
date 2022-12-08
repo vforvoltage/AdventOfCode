d72f4b53aa3b1efc1551077aa977b63ca2e683b7
vforvoltage <vforvoltage@gmail.com>
2022-12-08 18:21:42 -0500

    day 1 cleanup

--- src/main/java/org/vforvoltage/adventofcode/year2022/days/Day1.java
+++ src/main/java/org/vforvoltage/adventofcode/year2022/days/Day1.java
@@ -3,7 +3,6 @@ package org.vforvoltage.adventofcode.year2022.days;
 import org.vforvoltage.adventofcode.year2022.Day2022;
 
 import java.util.Arrays;
-import java.util.stream.IntStream;
 
 import static java.util.Collections.reverseOrder;
 
@@ -14,17 +13,21 @@ public class Day1 extends Day2022 {
 
     @Override
     public Object part1() {
-        return getElfTotals().max().orElseThrow();
+        return Arrays.stream(getTodaysInput().split("\n\n"))
+                .map(String::lines)
+                .mapToInt(s -> s.mapToInt(Integer::parseInt).sum())
+                .max()
+                .orElseThrow();
     }
 
     @Override
     public Object part2() {
-        return getElfTotals().boxed().sorted(reverseOrder()).limit(3).mapToInt(Integer::intValue).sum();
-    }
-
-    private IntStream getElfTotals() {
         return Arrays.stream(getTodaysInput().split("\n\n"))
-                .map(s -> s.lines().mapToInt(Integer::parseInt))
-                .mapToInt(IntStream::sum);
+                .map(String::lines)
+                .map(s -> s.mapToInt(Integer::parseInt).sum())
+                .sorted(reverseOrder())
+                .limit(3)
+                .mapToInt(Integer::intValue)
+                .sum();
     }
 }
